e22cc838553ce1b523488391e9d178e8688676ee
saguywalker <saguywalker@protonmail.com>
2025-09-14 11:54:04 +0900

    add /budgets and test

--- test/budgie/tracking_test.exs
+++ test/budgie/tracking_test.exs
@@ -1,6 +1,8 @@
 defmodule Budgie.TrackingTest do
   use Budgie.DataCase
 
+  import Budgie.TrackingFixtures
+
   alias Budgie.Tracking
 
   describe "budgets" do
@@ -9,13 +11,7 @@ defmodule Budgie.TrackingTest do
     test "create_budget/2 with valid data creates budget" do
       user = Budgie.AccountsFixtures.user_fixture()
 
-      valid_attrs = %{
-        name: "some name",
-        description: "some description",
-        start_date: ~D[2023-01-01],
-        end_date: ~D[2023-01-31],
-        creator_id: user.id
-      }
+      valid_attrs = valid_budget_attributes(%{creator_id: user.id})
 
       assert {:ok, %Budget{} = budget} = Tracking.create_budget(valid_attrs)
       assert budget.name == "some name"
@@ -26,14 +22,7 @@ defmodule Budgie.TrackingTest do
     end
 
     test "create_budget/2 requires name" do
-      user = Budgie.AccountsFixtures.user_fixture()
-
-      attrs_without_name = %{
-        description: "some description",
-        start_date: ~D[2023-01-01],
-        end_date: ~D[2023-01-31],
-        creator_id: user.id
-      }
+      attrs_without_name = valid_budget_attributes() |> Map.delete(:name)
 
       assert {:error, %Ecto.Changeset{} = changeset} = Tracking.create_budget(attrs_without_name)
 
@@ -42,15 +31,9 @@ defmodule Budgie.TrackingTest do
     end
 
     test "create_budget/2 requires valid dates" do
-      user = Budgie.AccountsFixtures.user_fixture()
-
-      attrs_with_invalid_dates = %{
-        name: "some name",
-        description: "some description",
-        start_date: ~D[2023-01-31],
-        end_date: ~D[2023-01-01],
-        creator_id: user.id
-      }
+      attrs_with_invalid_dates =
+        valid_budget_attributes()
+        |> Map.merge(%{start_date: ~D[2023-01-31], end_date: ~D[2023-01-01]})
 
       assert {:error, %Ecto.Changeset{} = changeset} =
                Tracking.create_budget(attrs_with_invalid_dates)
